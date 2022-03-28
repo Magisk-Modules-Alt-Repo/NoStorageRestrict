6279c725c932912ff8d6aea454e0a72bce171390
DanGLES3 <switchtegra@gmail.com>
2022-03-28 00:05:12 -0300

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,20 @@
+
+
+CODENAME=$(getprop ro.build.version.sdk)
+if [[ "$CODENAME" == "32" ]]; then
+  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
+  cp -rf "$MODPATH"/S2/* "$MODPATH"/system/priv-app/ExternalStorageProvider
+elif [[ "$CODENAME" == "31" ]]; then
+  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
+  cp -rf "$MODPATH"/S/* "$MODPATH"/system/priv-app/ExternalStorageProvider
+elif [[ "$CODENAME" == "30" ]]; then
+  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
+  cp -rf "$MODPATH"/R/* "$MODPATH"/system/priv-app/ExternalStorageProvider
+fi
+ 
+rm -rf "$MODPATH"/R
+rm -rf "$MODPATH"/S
+
+REPLACE="
+/system/priv-app/ExternalStorageProvider
+"
