e39640451b1a947d5f277cb99b1effada3860147
DanGLES3 <32078304+DanGLES3@users.noreply.github.com>
2023-01-23 13:23:45 -0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,17 +1,4 @@
-if [ -n "$MMM_EXT_SUPPORT" ]; then
-  ui_print "#!useExt"
-  mmm_exec() {
-    ui_print "$(echo "#!$@")"
-  }
-else
-  mmm_exec() { true; }
-  abort "! This module need to be executed in Fox's Magisk Module Manager"
-  exit 1
-fi
 
-ui_replace() {
-  mmm_exec setLastLine "$1"
-}
 
 CODENAME=$(getprop ro.build.version.sdk)
 if [[ "$CODENAME" == "33" ]]; then
@@ -36,9 +23,3 @@ rm -rf "$MODPATH"/T
 REPLACE="
 /system/priv-app/ExternalStorageProvider
 "
-
-ui_print ""
-ui_print "- Telegram Support group (top right corner)"
-ui_print ""
-
-mmm_exec setSupportLink "https://t.me/dnmgsk"
